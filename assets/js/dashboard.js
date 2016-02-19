0dcd279daa96fd6936d67d2b208b1dc67dde8bc5
Sofiane <theabstractdev@gmail.com>
2016-02-19 13:07:28 +0100

    fixed stuff + standard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,4 +1,4 @@
-var content = {}
+/* globals $, document */
 var reach
 var clicks
 var ie
@@ -7,125 +7,98 @@ var cost
 var postURL
 var embedKey
 
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-  results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+function getParameterByName (name, url) {
+  if (!url) url = window.location.href
+  name = name.replace(/[\[\]]/g, '\\$&')
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  var results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
-function getContent (data) {
-  if (data.feed.entry) {
-    var spreadsheetData = data.feed.entry[0].content.$t
-
-    var re = /(\w+): (.+?(?=(?:, \w+:|$)))/mgi;
-    var contentArray = re.exec(spreadsheetData)
-    while (contentArray != null) {
-      var propName = contentArray[1]
-      var propValue = contentArray[2]
-
-      content[propName] = propValue
-      contentArray = re.exec(spreadsheetData)
-    }
-
-    if (content["posturl"] || content["embedkey"] || content["reach"] || content["clicks"] || content["incomegenerated"] || content["cost"]) {
-      postURL = content["posturl"]
-      embedKey = content["embedkey"]
-      reach = content["reach"]
-      clicks = content["clicks"]
-      ie = content["incomegenerated"]
-      engagement = content["engagement"]
-      cost = content["cost"]
-    } else if (!content["posturl"] && content["embedkey"] && content["reach"] && content["clicks"] && content["incomegenerated"] && content["cost"]) {
-      document.location = '/error'
-    }
-  } else {
-      document.location = '/error'
-  }
-}
-
-$(document).ready(function() {
-
+$(document).ready(function () {
   $('.dashboard-name').hide()
   $('.dashboard-detail').children().hide()
   var dashboardID = getParameterByName('dashboard-id')
   var reportID = getParameterByName('report-id')
-  var URL = 'https://spreadsheets.google.com/feeds/list/' + reportID + '/' + dashboardID + '/public/basic?alt=json-in-script'
-  
+  var URL = 'https://spreadsheets.google.com/feeds/list/' + reportID + '/' + dashboardID + '/public/full?alt=json-in-script'
+
   $.ajax({
     url: URL,
     type: 'GET',
     dataType: 'jsonp',
-    jsonp: "callback",
+    jsonp: 'callback'
   })
-  .done(function (data) {
-    getContent(data)
-    var email = data.feed.author[0].email.$t.substr(-11)
-    if (email !== "@viewrz.com") document.location = '/error'
-    
-    if (ie) ie = '$' + ie.replace('$', '')
-    if (cost) cost = '$' + cost.replace('$', '')
-    if (engagement) engagement = Math.round(100 * engagement) + '%'
-    
-    if (embedKey) {
-      if (/[a-zA-Z0-9-_]+$/.test(embedKey) && embedKey.indexOf("/") > -1) {
-        $('.embed').html('<div class="tumblr-post" data-href="https://embed.tumblr.com/embed/post/' + embedKey + '"></div><script async src="https://secure.assets.tumblr.com/post.js"></script>')
+    .done(function (data) {
+      if (data.feed.entry[0].gsx$posturl) postURL = data.feed.entry[0].gsx$posturl.$t
+      if (data.feed.entry[0].gsx$embedkey) embedKey = data.feed.entry[0].gsx$embedkey.$t
+      if (data.feed.entry[0].gsx$reach) reach = data.feed.entry[0].gsx$reach.$t
+      if (data.feed.entry[0].gsx$clicks) clicks = data.feed.entry[0].gsx$clicks.$t
+      if (data.feed.entry[0].gsx$incomegenerated) ie = data.feed.entry[0].gsx$incomegenerated.$t
+      if (data.feed.entry[0].gsx$engagement) engagement = data.feed.entry[0].gsx$engagement.$t
+      if (data.feed.entry[0].gsx$cost) cost = data.feed.entry[0].gsx$cost.$t
+
+      var email = data.feed.author[0].email.$t.substr(-11)
+      if (email !== '@viewrz.com') document.location = '/error'
+      if (ie) ie = '$' + ie.replace('$', '')
+      if (cost) cost = '$' + cost.replace('$', '')
+      if (engagement) engagement = Math.round(100 * engagement) + '%'
+      if (embedKey) {
+        if (/[a-zA-Z0-9-_]+$/.test(embedKey) && embedKey.indexOf('/') > -1) {
+          $('.embed').html('<div class="tumblr-post" data-href="https://embed.tumblr.com/embed/post/' + embedKey + '"></div><script async src="https://secure.assets.tumblr.com/post.js"></script>')
+        }
       }
-    } 
+      if (!reach || /[a-zA-Z]/.test(reach)) $('#reach').parent().parent().remove()
+      if (!clicks || /[a-zA-Z]/.test(clicks)) $('#clicks').parent().parent().remove()
+      if (!engagement || /[a-zA-Z]/.test(engagement)) $('#engagement').parent().parent().remove()
+      if (!ie || /[a-zA-Z]/.test(ie)) $('#ie').parent().parent().remove()
+      if (!cost || /[a-zA-Z]/.test(cost)) $('#cost').parent().parent().remove()
 
-    if (!reach || /[a-zA-Z]/.test(reach)) $("#reach").parent().parent().remove()
-    if (!clicks || /[a-zA-Z]/.test(clicks)) $("#clicks").parent().parent().remove()
-    if (!engagement || /[a-zA-Z]/.test(engagement)) $("#engagement").parent().parent().remove()
-    if (!ie || /[a-zA-Z]/.test(ie)) $("#ie").parent().parent().remove()
-    if (!cost || /[a-zA-Z]/.test(cost)) $("#cost").parent().parent().remove()
+      if (!$('#reach').html() && !$('#clicks').html() && !$('#engagement').html() && !$('#ie').html() && !$('#cost').html()) {
+        $('.dashboard').css('height', 'auto')
+      }
 
-    if (!$('#reach').html() && !$('#clicks').html() && !$('#engagement').html() && !$('#ie').html() && !$('#cost').html()) {
-      $('.dashboard').css('height', 'auto')
-    }
+      if (postURL) {
+        $('.dashboard-name').html(data.feed.title.$t + ' <a class="post-link" href="' + postURL + '">Post link</a>')
+      } else {
+        $('.dashboard-name').html(data.feed.title.$t)
+        $('.dashboard-name').css('padding-left', '0')
+      }
 
-    if (postURL) {
-      $('.dashboard-name').html(data.feed.title.$t + ' <a class="post-link" href="' + postURL + '">Post link</a>')
-    } else {
-      $('.dashboard-name').html(data.feed.title.$t)
-      $('.dashboard-name').css('padding-left', '0');
-    }
+      $('#reach').html(reach)
+      $('#clicks').html(clicks)
+      $('#engagement').html(engagement)
+      $('#ie').html(ie)
+      $('#cost').html(cost)
 
-    $('#reach').html(reach)
-    $('#clicks').html(clicks)
-    $('#engagement').html(engagement)
-    $('#ie').html(ie)
-    $('#cost').html(cost)
+      if ($('.dashboard-section').length === 4) {
+        $('.dashboard-section').css('width', '24.2%')
+        $('.dashboard-section').addClass('four')
+      }
 
-    if ($('.dashboard-section').length === 4) {
-      $('.dashboard-section').css('width', '24.2%')
-      $('.dashboard-section').addClass('four')
-    }
+      if ($('.dashboard-section').length === 3) {
+        $('.dashboard-section').css('width', '33.1%')
+        $('.dashboard-section').addClass('three')
+      }
 
-    if ($('.dashboard-section').length === 3) {
-      $('.dashboard-section').css('width', '33.1%')
-      $('.dashboard-section').addClass('three')
-    }
+      if ($('.dashboard-section').length === 2) {
+        $('.dashboard-section').css('width', '49.2%')
+        $('.dashboard-section').addClass('two')
+      }
 
-    if ($('.dashboard-section').length === 2) {
-      $('.dashboard-section').css('width', '49.2%')
-      $('.dashboard-section').addClass('two')
-    }
+      if ($('.dashboard-section').length === 1) {
+        $('.dashboard-section').css('width', '101%')
+        $('.dashboard-section').addClass('one')
+      }
+      var date = new Date(data.feed.entry[0].updated.$t).toString()
+      date = date.split(':')[0] + ':' + date.split(':')[1]
 
-    if ($('.dashboard-section').length === 1) {
-      $('.dashboard-section').css('width', '101%')
-      $('.dashboard-section').addClass('one')
-    }
-    var date = new Date(data.feed.entry[0].updated.$t).toString()
-    date = date.split(':')[0] + ':' + date.split(':')[1]
-
-    $('.last-update').html('Last update: ' + date)
-    $('.dashboard-name').show()
-    $('.dashboard-detail').children().show()
-  })
-  .fail(function () {
-    document.location = '/error'
-  })
+      $('.last-update').html('Last update: ' + date)
+      $('.dashboard-name').show()
+      $('.dashboard-detail').children().show()
+    })
+    .fail(function () {
+      document.location = '/error'
+    })
 })
--- assets/js/report.js
+++ assets/js/report.js
@@ -1,37 +1,35 @@
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-  results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+/* globals $, document */
+function getParameterByName (name, url) {
+  if (!url) url = window.location.href
+  name = name.replace(/[\[\]]/g, '\\$&')
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  var results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   if (!getParameterByName('report-id')) document.location = '/error'
   var reportID = getParameterByName('report-id')
-  var URL = 'https://spreadsheets.google.com/feeds/worksheets/' + reportID + '/public/basic?alt=json-in-script'
+  var URL = 'https://spreadsheets.google.com/feeds/worksheets/' + reportID + '/public/full?alt=json-in-script'
 
   $.ajax({
     url: URL,
     type: 'GET',
     dataType: 'jsonp',
-    jsonp: "callback",
+    jsonp: 'callback'
   })
-  .done(function (data) {
-    var email = data.feed.author[0].email.$t.substr(-11)
-    if (email !== "@viewrz.com") document.location = '/error'
-    
-    $('.report-name').html(data.feed.title.$t)
-    
-    for (var i = 0; i < data.feed.entry.length; i++) {
-      $('.list').append('<a class=list-link href="dashboard.html?report-id=' + reportID + '&dashboard-id=' + data.feed.entry[i].id.$t.split('/basic/')[1] + '">' + data.feed.entry[i].title.$t + '<span class="arrow">></span></a>')
-    }
-
-    $('.list-link').first().css('margin-top', '15px')
-  })
-  .fail(function () {
-    document.location = '/error'
-  })
+    .done(function (data) {
+      var email = data.feed.author[0].email.$t.substr(-11)
+      if (email !== '@viewrz.com') document.location = '/error'
+      $('.report-name').html(data.feed.title.$t)
+      for (var i = 0; i < data.feed.entry.length; i++) {
+        $('.list').append('<a class=list-link href="dashboard.html?report-id=' + reportID + '&dashboard-id=' + data.feed.entry[i].id.$t.split('/full/')[1] + '">' + data.feed.entry[i].title.$t + '<span class="arrow">></span></a>')
+      }
+      $('.list-link').first().css('margin-top', '15px')
+    })
+    .fail(function () {
+      document.location = '/error'
+    })
 })
